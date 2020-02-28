cff056ad357e103969db3b3a61fa34c29294b802
KainDorris <KainDorris95@gmail.com>
2020-02-27 18:03:37 -0600

    MainMenu Setup with SceneLoader

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+
+    public void LoadGame(){
+        SceneManager.LoadScene("Game");
+    }
+
+    public void LoadOptions(){
+        SceneManager.LoadScene("Options");
+    }
+    public void QuitGame(){
+        Application.Quit();
+    }
+}
