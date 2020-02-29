7dd097a40d37bdd7a29089dd1cbb396ebb365bce
KainDorris <KainDorris95@gmail.com>
2020-02-28 21:35:29 -0600

    Add save system

---
+++ Assets/Scripts/PlayerData.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class PlayerData 
+{
+    public int playerLevel;
+    public int playerGold;
+    public ArrayList playerProgression;
+
+    //Constructor, takes data from save system and jams it in there
+    public PlayerData (Player player){
+        playerLevel = player.level;
+        playerGold = player.gold;
+
+    }
+}
---
+++ Assets/Scripts/SaveSystem.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+public static class SaveSystem 
+{
+
+    //configs, where to save the data at for each element
+    public static string playerSavePath = Application.persistentDataPath + "/player.state";
+
+
+    //Save player function,
+    // Takes a player object that asumes some data points
+    // Creates a binary formatter to save the data
+    // Opens the file stream to write to a file at the specified path
+    // Converts the data into binary and then writes it to the file
+    //Closes the stream because that's what Brackey's told me
+    public static void SavePlayer(Player player){
+        BinaryFormatter formatter = new BinaryFormatter();
+        FileStream stream = new FileStream(playerSavePath, FileMode.Create);
+        PlayerData data = new PlayerData(player);
+        formatter.Serialize(stream, data);
+        stream.Close();
+
+
+
+
+    }
+
+
+    //PlayerLoad function
+    //Does basically the same as the save function but opens the file and reads from it. 
+    //Hands data to the PlayerData Constructor function since it's static and can be called from there (player data script)
+    public static PlayerData LoadPlayer(){
+        if(File.Exists(playerSavePath)){
+            BinaryFormatter formatter = new BinaryFormatter;
+            FileStream stream = new FileStream(playerSavePath, FileMode.Open);
+            PlayerData data = formatter.Deserialize(stream) as PlayerData;
+            stream.Close();
+            return data;
+            
+        }
+        else{
+            Debug.LogError("Save file not found at" + playerSavePath);
+            return null;
+        }
+
+    }
+    
+}
