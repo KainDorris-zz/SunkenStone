7fa977829d49014204a49528c57e15c1326b73e1
Joshua Morris <jrem94@live.com>
2020-02-29 22:48:14 -0600

    Added Enemy, Sound, and Script

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private float health;
+    [SerializeField] private float speed = 4f;
+    [SerializeField] private float attackSpeed = 1;
+    [SerializeField] private float attackCoolDown = 2f;
+    [SerializeField] private float damage = 10f;
+    [SerializeField] private float stopDistance = 1f;
+    [SerializeField] private AudioSource audioSource;
+    private Player _player;
+    private float _attackTimer;
+
+    private void Start()
+    {
+        _player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+    }
+
+    private void Update()
+    {
+        FollowPlayer();
+    }
+
+    private void FollowPlayer()
+    {
+        if (_player != null)
+        {
+            Vector2 playerTransform = _player.transform.position;
+            if (Vector2.Distance(transform.position, playerTransform) > stopDistance)
+            {
+                transform.position = Vector2.MoveTowards(transform.position,
+                    playerTransform, speed * Time.deltaTime);
+            }
+            else
+            {
+                if (Time.time >= _attackTimer)
+                {
+                    StartCoroutine(AttackRoutine());
+                    _attackTimer = Time.time + attackCoolDown;
+                }
+            }
+        }
+    }
+
+    private IEnumerator AttackRoutine()
+    {
+        // _player.TakeDamage(damage);
+        float animationPercent = 0;
+        audioSource.Play();
+        while (animationPercent <= 1)
+        {
+            animationPercent += Time.deltaTime * attackSpeed;
+            // Trigger Animation here.
+            Debug.Log("Animating!");
+            yield return null;
+        }
+        audioSource.Stop();
+        Debug.Log("Animation Done");
+    }
+
+    public void TakeDamage(float dmg)
+    {
+        health -= dmg;
+
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
