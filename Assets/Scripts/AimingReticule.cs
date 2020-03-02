92b496ea9e7e948787aa591c5341e3e35067e7a8
KainDorris <KainDorris95@gmail.com>
2020-03-01 18:45:53 -0600

    Initial version

---
+++ Assets/Scripts/AimingReticule.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AimingReticule : MonoBehaviour
+{
+    private Vector2 mousePosition;
+
+    private void Start(){
+        transform.position = Input.mousePosition;
+        
+    }
+
+    private void Update(){
+        UpdateReticulePosition();
+
+    }
+
+    private void UpdateReticulePosition(){
+        transform.position = Input.mousePosition;
+    }
+
+    
+}
