1e1b9e813e9be0375176685769e73fbcccbf387e
KainDorris <KainDorris95@gmail.com>
2020-03-01 20:22:23 -0600

    add aiming reticule report in world space

--- Assets/Scripts/AimingReticule.cs
+++ Assets/Scripts/AimingReticule.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class AimingReticule : MonoBehaviour
 {
     private Vector2 mousePosition;
+    private Vector2 CameraPositionInWorld;
 
     private void Start(){
         transform.position = Input.mousePosition;
@@ -18,6 +19,13 @@ public class AimingReticule : MonoBehaviour
 
     private void UpdateReticulePosition(){
         transform.position = Input.mousePosition;
+        GetCursorPositionInWorld();
+    }
+
+    public Vector2 GetCursorPositionInWorld(){
+        CameraPositionInWorld = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Debug.Log(CameraPositionInWorld);
+        return CameraPositionInWorld;
     }
 
     
