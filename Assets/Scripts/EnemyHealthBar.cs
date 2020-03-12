9b12a56db43b6d1141081f68e2d860b1da41b1d7
KainDorris <KainDorris95@gmail.com>
2020-03-11 19:20:13 -0500

    Add enemy health bar

---
+++ Assets/Scripts/EnemyHealthBar.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealthBar : MonoBehaviour
+{
+
+    [SerializeField] private Enemy enemy;
+    private float enemyHealthPercentage;
+    private float enemyMaxHealth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (enemy == null){
+            enemy = transform.root.GetComponent<Enemy>();
+        } 
+        enemyMaxHealth = enemy.GetCurrentHealth(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+          UpdateSize();
+    }
+
+
+
+    private void UpdateSize(){
+        //multiply by 2 because transform.scale.x = 2
+        enemyHealthPercentage =  enemy.GetCurrentHealth() / enemyMaxHealth * 2; 
+        gameObject.transform.localScale = new Vector3(enemyHealthPercentage, gameObject.transform.localScale.y, gameObject.transform.localScale.z);
+    }
+}
