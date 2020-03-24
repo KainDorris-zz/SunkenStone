29363357bf474b161c21606ec3db4b9a825f25bb
KainDorris <KainDorris95@gmail.com>
2020-03-23 19:41:18 -0500

    Include spawner that creates enemies at set intervals based on prefabs in a list

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] List<GameObject> enemies;
+    [SerializeField] int roundsToSpawn = 5;
+    [SerializeField] float spawnTimer = 5f;
+    //TODO Randomize spawning
+    //[SerializeField, Tooltip("Spawn enemies randomly from list")] bool randomSpawning = false;
+    
+    private void Start() {
+        StartCoroutine("SpawnEnemies");
+    }
+
+    //Loop through N times where N= rounds to spawn
+    private IEnumerator SpawnEnemies(){
+
+        for (int i = 0; i < roundsToSpawn; i++){
+            for (int j = 0; j < enemies.Count; j++){
+            
+            Instantiate(enemies[i], gameObject.transform);                       
+            Debug.Log("Spawning enemy" + enemies[j]);
+            yield return new WaitForSeconds(spawnTimer);
+            }
+
+        }
+        
+    }
+}
