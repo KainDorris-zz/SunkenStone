3eeeff96791e5a91152e347f4207cc58f7975c22
KainDorris <KainDorris95@gmail.com>
2020-04-02 20:07:49 -0500

    Add minion db and scriptable object base scripts

---
+++ Assets/Scripts/MinionDatabase.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "MinionDatabase", menuName = "SunkenStone/MinionDatabase", order = 0)]
+public class MinionDatabase : ScriptableObject {
+    [SerializeField] List<ScriptableObject> minionDatabase;
+}
---
+++ Assets/Scripts/MinionScriptableObject.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "MinionScriptableObject", menuName = "SunkenStone/MinionScriptableObject", order = 0)]
+public class MinionScriptableObject : ScriptableObject {
+    
+    [SerializeField] public string minionName;
+    [SerializeField] public string minionDescription;
+    
+    [SerializeField] public int health;
+    [SerializeField] public float damage;
+}
